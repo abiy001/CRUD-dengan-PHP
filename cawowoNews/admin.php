0215483bc5f472f54d643a2b315409744eabcfc4
abiy001 <hafidhabiyyu01@gmail.com>
2024-05-06 19:28:50 +0700

    nah

--- cawowoNews/admin.php
+++ cawowoNews/admin.php
@@ -20,10 +20,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+       
+    </style>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-<div class='flex items-center'>
+    <div class="bg-slate-700 h-screen">
+
+        <nav class="flex justify-between items-center bg-blue-950 py-3 text-slate-50 w-screen" id='navbar'>
+            <h1 class="font-bold ml-4">CAWOWO NEWS</h1>
+        <div class="font-semibold mx-10">
+        <a class="mx-3" href="#">Olahraga</a>
+        <a class="mx-3" href="#">Politik</a>
+        <a class="mx-3" href="#">Teknologi</a>
+        <a class="mx-3" href="#">Pendidikan</a>
+    </div>
+</nav>
+<div class="text-blue-950 font-semibold bg-orange-100 py-1.5 text-2xl ">
+    <div class='flex flex-col'>
+        <h3>BREAKING</h3>
+        <h4 class="text-red-500 ml-16 font-semibold">NEWS</h4>
+</div> 
+</div>
+
+
+
+<div class='flex items-center py-20'>
 <div class='grid gap-4 place-content-center grid-cols-4 w-screen w-80 p-2 '>
     <?php
         foreach ($data_news['items'] as $value) {?>
@@ -55,6 +78,11 @@
 }
 ?>
 </div>
+</div>
+
+<footer class="bg-blue-950 flex text-orange-100 justify-center items-center" >
+    <h1>privacy & enkripsi</h1>
+</footer>
 </div>
 </body>
 </html>
